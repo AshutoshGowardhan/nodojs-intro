2f34a9c9f2f15a38524e8c39a5ab3d0599408976
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-09 21:41:05 +0530

    checking and deleting the folder

--- fileSystem.js
+++ fileSystem.js
@@ -3,28 +3,37 @@ const fs = require('fs');
 // console.log(fs)
 
 // reading files, this is asynchronous, it will not block the code
-fs.readFile('./docs/file.txt', (err, data) => {
-    if (err) {
-        console.log(err)
-    }
-    console.log(data)
-    console.log(data.toString())
-})
+// fs.readFile('./docs/file.txt', (err, data) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     console.log(data)
+//     console.log(data.toString())
+// })
 
 
-// writing to file
-fs.writeFile('./docs/blog1.txt', 'hello world', () => {
-    console.log('file was written')
-})
+// // writing to file
+// fs.writeFile('./docs/blog1.txt', 'hello world', () => {
+//     console.log('file was written')
+// })
 
-fs.writeFile('./docs/blog2.txt', 'hello world, no file exits', () => {
-    console.log('file was written')
-})
+// fs.writeFile('./docs/blog2.txt', 'hello world, no file exits', () => {
+//     console.log('file was written')
+// })
 
 //Directories
-fs.mkdir('./assests', (err) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log('folder created')
-})
+if (!fs.existsSync('./assests')) {
+    fs.mkdir('./assests', (err) => {
+        if (err) {
+            console.log(err);
+        }
+        console.log('folder created')
+    })
+} else {
+    fs.rmdir('./assests', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log('folder deleted')
+    })
+}
