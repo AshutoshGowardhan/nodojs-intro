25b2b30c1ae2654a165bea1f668a639ea003979f
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-09 21:35:37 +0530

    writing files

--- fileSystem.js
+++ fileSystem.js
@@ -2,11 +2,21 @@ const fs = require('fs');
 
 // console.log(fs)
 
-// reading files 
+// reading files, this is asynchronous, it will not block the code
 fs.readFile('./docs/file.txt', (err, data) => {
     if (err) {
         console.log(err)
     }
     console.log(data)
     console.log(data.toString())
+})
+
+
+// writing to file
+fs.writeFile('./docs/blog1.txt', 'hello world', () => {
+    console.log('file was written')
+})
+
+fs.writeFile('./docs/blog2.txt', 'hello world, no file exits', () => {
+    console.log('file was written')
 })
