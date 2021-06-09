530bef19d43a8a366eee3bde5fdae3f91858e912
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-09 22:38:10 +0530

    sending response setting heeader

--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method)
+    // console.log(req.url, req.method)
+
     console.log('request made')
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>hello, ninjas</h1>')
+    res.write('<h2>hello, ninjas, second</h2>')
+    res.end()
 });
 
 server.listen(3001, 'localhost', () => {
