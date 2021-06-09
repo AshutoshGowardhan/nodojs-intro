c3b89cb1a4860bc515033dbac715c3d2d9779ae3
Ashutosh Gowardhan <ashutosh.reso@gmail.com>
2021-06-09 23:08:03 +0530

    sending html pages as response

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     // console.log(req.url, req.method)
@@ -7,9 +8,18 @@ const server = http.createServer((req, res) => {
 
     // set header content type
     res.setHeader('Content-Type', 'text/html')
-    res.write('<h1>hello, ninjas</h1>')
-    res.write('<h2>hello, ninjas, second</h2>')
-    res.end()
+    // res.write('<h1>hello, ninjas</h1>')
+    // res.write('<h2>hello, ninjas, second</h2>')
+    // send the html file
+    fs.readFile('../views/index.html', (err, data) => {
+        if (err) {
+            console.log(err)
+            res.end();
+        } else {
+            //res.write(data)
+            res.end(data);
+        }
+    })
 });
 
 server.listen(3001, 'localhost', () => {
